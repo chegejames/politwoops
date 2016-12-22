e4926f1edf80dd988e10103b868efa544f74ce7e
Derek Willis <derek.willis@propublica.org>
2016-12-21 21:31:03 -0500

    admin css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(propublica_base/base.css propublica_base/master.css propublica_base/print.css propublica_base/woland.css)
+Rails.application.config.assets.precompile += %w(admin.css propublica_base/base.css propublica_base/master.css propublica_base/print.css propublica_base/woland.css)
