93c82d2bfad948928515800cf37f27d28c288eb0
chege <chegejames3@gmail.com>
2017-05-19 19:53:28 +0300

    log unicorn

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -3,6 +3,9 @@ working_directory "/web/"
 listen 80
 preload_app true
 
+stderr_path "/web/log/unicorn.stderr.log"
+stdout_path "/web/log/unicorn.stdout.log"
+
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 end
