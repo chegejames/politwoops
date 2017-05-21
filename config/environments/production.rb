9784bcef8538583116e479a0ee0724d81094038b
chege <chegejames3@gmail.com>
2017-05-21 17:24:21 +0300

    compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,13 +7,13 @@ Politwoops::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
-  config.log_level = :info
+  config.log_level = :debug
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
@@ -28,17 +28,17 @@ Politwoops::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  #config.cache_store = :mem_cache_store,
-  #  "newsapps-vpc.o2zdh0.0001.use1.cache.amazonaws.com",
-  #  { :namespace => 'politwoops', compress: true }
+  config.cache_store = :mem_cache_store,
+    "newsapps-vpc.o2zdh0.0001.use1.cache.amazonaws.com",
+    { :namespace => 'politwoops', compress: true }
     #{ :namespace => 'politwoops', expires_in: 1.day, compress: true }
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  #config.serve_static_files = false
+  config.serve_static_files = false
 
-  #config.action_controller.asset_host = "https://static.propublica.org"
-  #config.assets.prefix = "/rails/assets/politwoops"
+  config.action_controller.asset_host = "https://static.propublica.org"
+  config.assets.prefix = "/rails/assets/politwoops"
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
