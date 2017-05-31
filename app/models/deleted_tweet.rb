9be0d1bd85b920d83735a261bc56b9ca19a8d690
chege <chegejames3@gmail.com>
2017-05-31 16:26:50 +0300

    approve deleted tweets by default

--- app/models/deleted_tweet.rb
+++ app/models/deleted_tweet.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 class DeletedTweet < Tweet
   self.table_name="deleted_tweets"
-#default values set by python worker
+#default values for approved tweets set by python worker
 end
